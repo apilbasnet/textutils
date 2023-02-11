a8ae8d1fe8cbb20782ce120a4b716707790bb1f4
apilbasnet <117279433+apilbasnet@users.noreply.github.com>
2023-02-11 10:42:27 +0545

    added uppercase and lowercase algorithms

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/textForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <Navbar title="Text Utility" about="About Us " homePage="Home" />
+    <>
+      <div>
+        <Navbar title="Text Utility" about="About Us " homePage="Home" />
 
-      <TextForm heading="Enter your text " />
+        <TextForm heading="Enter your text " />
 
-      <button className="button ">Change to UpperCase</button>
-    </div>
+
+      </div>
+    </>
   );
 }
 
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,11 +1,49 @@
+import { useState } from "react";
 import React from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter your text.");
+
+  const UpClick = () => {
+    console.log("UPClick was pressed.");
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const lowClick = () => {
+    console.log("LowClick was pressed.");
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+
+
+  const handleOnChange = (event) => {
+    console.log("onchange");
+    setText(event.target.value)
+  }
+
   return (
     <>
       <h1>{props.heading}</h1>
       <div className="myBox">
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          id="myBox"
+          rows="8"
+          onChange={handleOnChange}
+        ></textarea>
+
+
+        <button className="button" onClick={UpClick} >
+          Change to UpperCase
+        </button>
+
+        <button className="button" onClick={lowClick} >
+          Change to LowerCase
+        </button>
+
       </div>
     </>
   );
